eaebd27a0498889427f138e9770e25daf65de91f
Dominic Maglione <dcmaglione@gmail.com>
2022-02-13 13:53:39 -0500

    post request to front end for login; fixed sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE Photoshare;
 USE Photoshare;
 
-CREATE TABLE User (
+CREATE TABLE Users (
   user_id int4 AUTO_INCREMENT,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
@@ -16,40 +16,40 @@ CREATE TABLE User (
 CREATE TABLE Are_Friend (
   friend_a int4,
   friend_b int4,
-  CONSTRAINT friend_a FOREIGN KEY (friend_a) REFERENCES User(user_id),
-  CONSTRAINT friend_b FOREIGN KEY (friend_b) REFERENCES User(user_id)
+  CONSTRAINT friend_a FOREIGN KEY (friend_a) REFERENCES Users(user_id),
+  CONSTRAINT friend_b FOREIGN KEY (friend_b) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Album (
+CREATE TABLE Albums (
   album_id int4 AUTO_INCREMENT,
   user_id int4,
   album_name VARCHAR(255),
   created DATETIME,
   CONSTRAINT album_pk PRIMARY KEY (album_id),
-  FOREIGN KEY (user_id) REFERENCES User (user_id)  
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)  
 );
 
-CREATE TABLE Photo (
+CREATE TABLE Photos (
   photo_id int4 AUTO_INCREMENT,
   album_id int4,
   caption VARCHAR(255),
   data VARCHAR(255),
   CONSTRAINT photo_pk PRIMARY KEY (photo_id),
-  CONSTRAINT album_id FOREIGN KEY (album_id) REFERENCES Album(album_id)
+  CONSTRAINT album_id FOREIGN KEY (album_id) REFERENCES Albums(album_id)
 );
 
-CREATE TABLE Comment (
+CREATE TABLE Comments (
   comment_id int4 AUTO_INCREMENT,
   user_id int4,
   photo_id int4,
   text VARCHAR(255) NOT NULL,
   date DATETIME,
   CONSTRAINT comment_pk PRIMARY KEY (comment_id),
-  FOREIGN KEY (user_id) REFERENCES User(user_id),
-  FOREIGN KEY (photo_id) REFERENCES Photo(photo_id)
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (photo_id) REFERENCES Photos(photo_id)
 );
 
-CREATE TABLE Tag (
+CREATE TABLE Tags (
   tag_id int4 AUTO_INCREMENT,
   tag_name VARCHAR(255),
   CONSTRAINT tag_pk PRIMARY KEY (tag_id)
@@ -58,11 +58,9 @@ CREATE TABLE Tag (
 CREATE TABLE Has_Tag (
   tag_id int4,
   photo_id int4,
-  CONSTRAINT tag_id FOREIGN KEY (tag_id) REFERENCES Tag(tag_id),
-  CONSTRAINT photo_id FOREIGN KEY (photo_id) REFERENCES Photo(photo_id)
+  CONSTRAINT tag_id FOREIGN KEY (tag_id) REFERENCES Tags(tag_id),
+  CONSTRAINT photo_id FOREIGN KEY (photo_id) REFERENCES Photos(photo_id)
 );
 
-INSERT INTO User (email, password) VALUES ('dcmag@bu.edu', 'dcmag');
-INSERT INTO User (email, password) VALUES ('ptrandev@bu.edu', 'ptrandev');
-
-DROP DATABASE Photoshare;
+INSERT INTO Users (email, password) VALUES ('dcmag@bu.edu', 'dcmag');
+INSERT INTO Users (email, password) VALUES ('ptrandev@bu.edu', 'ptrandev');
